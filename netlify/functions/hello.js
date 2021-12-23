85daa459db74b17c776d8096e6d2b0ec6bc8c0cc
iromashko <iromashko@me.com>
2021-12-23 11:03:34 +0300

    add sanity backend

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,10 +1,12 @@
 exports.handler = async (event, context) => {
+  const name = event.queryStringParameters.name || "Buddy";
+
   console.log(`hello!`);
   console.log(`event: ${JSON.stringify(event)}`);
   console.log(`context: ${JSON.stringify(context)}`);
 
   return {
     statusCode: 200,
-    body: "Hello!",
+    body: `Hello, ${name}`,
   };
 };
